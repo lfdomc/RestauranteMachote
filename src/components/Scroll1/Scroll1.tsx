233bdab7bb289c41f82c2f3e19df5bea99c5e697
Luis Morua <lfdomc@gmail.com>
2025-02-02 17:54:25 -0600

    commit15

--- src/components/Scroll1/Scroll1.tsx
+++ src/components/Scroll1/Scroll1.tsx
@@ -6,15 +6,15 @@ import "slick-carousel/slick/slick-theme.css";
 
 const SliderWrapper = styled.div`
   width: 100%;
-  overflow: hidden; // Evita cualquier espacio adicional
+  overflow: hidden;
 `;
 
 const SlideContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
-  padding: 10px;
+  padding: 0;
+  margin: 0;
 `;
 
 const Img = styled.img`
@@ -22,6 +22,11 @@ const Img = styled.img`
   width: 500px;
   object-fit: cover;
   border-radius: 10px;
+  margin: 0;
+  &:hover {
+    background-color: #821f82;
+    transform: scale(1.05);
+  }
 
   @media (max-width: 768px) {
     height: 450px;
@@ -30,15 +35,18 @@ const Img = styled.img`
 `;
 
 const settings = {
-  dots: true,
+  dots: false,
   infinite: true,
-  speed: 4800,
+  speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 4000,
-  centerMode: false, // Deshabilita el centrado que puede agregar margen
-  variableWidth: false, // Asegura que los slides se ajusten bien
+  autoplaySpeed: 3000,
+  centerMode: false,
+  variableWidth: true,
+  swipeToSlide: true,
+  touchThreshold: 10,
+  adaptiveHeight: true, // Agrega esta opción para evitar espacios vacíos
   responsive: [
     {
       breakpoint: 1024,
@@ -58,16 +66,7 @@ const settings = {
 const Scroll1: React.FC = () => (
   <SliderWrapper>
     <Slider {...settings}>
-      {[
-        "sopas",
-        "tacobirria",
-        "tacotico",
-        "nachos",
-        "ktripapas",
-        "fajitas",
-        "ktriburguer",
-        "patacones",
-      ].map((img, index) => (
+      {["sopas", "tacobirria", "tacotico", "nachos", "ktripapas", "fajitas", "ktriburguer", "patacones"].map((img, index) => (
         <SlideContainer key={index}>
           <Img src={`/image/ktrina/${img}.jpg`} alt={`Imagen ${index + 1}`} />
         </SlideContainer>
